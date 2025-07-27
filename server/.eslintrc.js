b56e7bccf8e9c8b0de8b32c561ebefdab7681dea
Omar Baltaji <omr.baltaji@gmail.com>
2025-07-27 13:50:18 +0300

    no need to install twice

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     'commonjs': true,
     'es2021': true,
     'node': true,
-    'jest/globals': true,
+    // 'jest/globals': true,
   },
   'extends': 'eslint:recommended',
   'parserOptions': {
